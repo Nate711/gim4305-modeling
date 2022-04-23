4216180ae5f154964d700361c8f1ac8493e991e5
Nate711 <nathankau@gmail.com>
2022-04-23 06:04:17 +0100

    repeat jumping and logging

--- pupperv3.py
+++ pupperv3.py
@@ -8,7 +8,39 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--torque', required=False, type=float, default=0.1)
+parser.add_argument('--jump_period', required=True, type=float, default=1.0)
+parser.add_argument('--stop', action='store_true', default=False)
+
 args = parser.parse_args()
+
+
+def calculate_progress(time_since_start, period):
+    return (time_since_start % period) / period
+
+
+def jump_land(progress,
+              jump_duration=0.2,
+              damp_duration=0.6,
+              reset_duration=0.2,
+              reset_pos=0.3,
+              land_pos=0.8,
+              jump_pos=1.4):
+    if progress < 0 or progress > 1:
+        raise RuntimeError(f"Progress should be in [0,1]. Is {progress}")
+    if progress < jump_duration:
+        pos = jump_pos
+        kp_scale = 6
+        kd_scale = 2
+    elif progress < jump_duration + damp_duration:
+        pos = land_pos
+        kp_scale = 0.2
+        kd_scale = 2
+    else:
+        pos = reset_pos
+        kp_scale = 6
+        kd_scale = 2
+
+    return (pos, kp_scale, kd_scale)
 
 
 async def main():
@@ -16,23 +48,31 @@ async def main():
         servo_bus_map={1: [1]},
     )
     controller = moteus.Controller(id=1, transport=transport)
-    print(controller)
+    if args.stop:
+      await controller.set_stop()
+      print("Stopped")
+      return
+
+    print("Controller: ", controller)
     data = []
     try:
         timestamp_begin_loop = time.time()
         while True:
             time_since_start = time.time() - timestamp_begin_loop
-            pos = 0.0#math.sin(time.time() * 12) * 0
+            progress = calculate_progress(time_since_start, args.jump_period)
+            (pos, kp_scale, kd_scale) = jump_land(progress)
             state = await controller.set_position(position=pos,
-                                                  kp_scale=6.0,
-                                                  kd_scale=2.0,
+                                                  kp_scale=kp_scale,
+                                                  kd_scale=kd_scale,
                                                   maximum_torque=args.torque,
                                                   query=True
                                                   )
             data.append((time_since_start, state))
             tau = state.values[moteus.Register.TORQUE]
+            read_pos = state.values[moteus.Register.POSITION]
             kt = 8.27 / 153
-            print(f"{args.torque} {tau:0.2}\t{tau / kt:0.2}")
+            print(
+                f"Max torque: {args.torque}\t Comm. torque: {tau:0.3f}\tCurrent: {tau / kt:0.2f}\tMotor pos: {read_pos:0.2f}\t Comm pos: {pos:0.2f}")
             await asyncio.sleep(0.01)
     finally:
         await controller.set_stop()
